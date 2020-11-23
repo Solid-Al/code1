9658d834a6d55bdfd809ca53dccb65a88597a224
Solid-Al <72567069+Solid-Al@users.noreply.github.com>
2020-11-23 10:25:37 -0500

    update

--- sentiment_analysis5.py
+++ sentiment_analysis5.py
@@ -1,33 +1,35 @@
 
-f = open('Data/keywords.txt')
-keyContents = f.read()
-keyContents = keyContents.split(',')
+def compute_tweets(tweets,keywords):
 
-f.close()
+    f = open('Data/keywords.txt')
+    keyContents = f.read()
+    keyContents = keyContents.split(',')
+
+#f.close()
 #print(keyContents)
 
-p = open('Data/tweets.txt')
-tweetContents = p.read()
+    p = open('Data/tweets.txt')
+    tweetContents = p.read()
 #tweetContents = tweetContents.split('\n')
-p.close()
+#p.close()
 
 #for line in keyContents:
 #      divide the line into word and value
 #      assign the values to variables
 #      dictionary[word] = value
 
-keyContents = {}
-for line in keyContents:
-    parts = line.lower()
-    parts = parts.split(",")
-    keyword = parts[0]
-    value = int(parts[1])
-    keyContents[keyword] = value
-    print(keyContents)
+    keyContents = {}
+    for line in keyContents:
+        parts = line.lower()
+        parts = parts.split(",")
+        keyword = parts[0]
+        value = int(parts[1])
+        keyContents[keyword] = value
+        print(keyContents)
 
-for line in tweetContents:
-    partsOne = line.split(" ",5)
+    for line in tweetContents:
+        partsOne = line.split(" ",5)
     #latitude = (float(partsOne[0].lstrip("[").rstrip(",")))
     #tweet = partsOne[5].lower().strip()
-    print(partsOne[0].lstrip("[").rstrip(","))
+        print(partsOne[0].lstrip("[").rstrip(","))
 #print(tweetContents)

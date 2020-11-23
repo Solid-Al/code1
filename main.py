aeb65ac2e259a145009d7979842c1ece12c3e9a6
Solid-Al <72567069+Solid-Al@users.noreply.github.com>
2020-11-23 08:34:13 -0500

    updates

---
+++ main.py
@@ -0,0 +1,37 @@
+
+f = open('keywords.txt')
+keyContents = f.read()
+keyContents = keyContents.split(',')
+
+f.close()
+#print(keyContents)
+
+p = open('tweets.txt')
+tweetContents = p.read()
+#tweetContents = tweetContents.split('\n')
+p.close()
+
+#for line in keyContents:
+#      divide the line into word and value
+#      assign the values to variables
+#      dictionary[word] = value
+
+keyContents = {}
+for line in keyContents:
+    parts = line.lower()
+    parts = parts.split(",")
+    keyword = parts[0]
+    value = int(parts[1])
+    keyContents[keyword] = value
+    print(keyContents)
+
+for line in tweetContents:
+    line = line.casefold().rstrip()
+    wordList = line.split()
+
+
+    #partsOne = line.split(" ",5)
+    #latitude = (float(partsOne[0].lstrip("[").rstrip(",")))
+    #tweet = partsOne[5].lower().strip()
+    print(partsOne[0].lstrip("[").rstrip(","))
+#print(tweetContents)
